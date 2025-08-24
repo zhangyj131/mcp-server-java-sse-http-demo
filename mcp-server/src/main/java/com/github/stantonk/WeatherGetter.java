1832be54ac0f439dc412cdbfedf13d411ea58ca3
zhangyj <zhangyongjie8810@zto.com>
2025-08-24 13:19:05 +0800

    支持springboot

--- mcp-server/src/main/java/com/github/stantonk/WeatherGetter.java
+++ mcp-server/src/main/java/com/github/stantonk/WeatherGetter.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
 
 import java.io.IOException;
 import java.net.URI;
